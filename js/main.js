7fbb0642e6570057020bf60f52148a3006ab7f2e
Victoria396 <95283353+Victoria396@users.noreply.github.com>
2022-09-06 10:46:36 +0700

    закончила функцианал всех секций, перехожу к меню

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// слайдер для табов
 const slickPreset = {
     infinite: true,
     slidesToShow: 2,
@@ -7,13 +8,25 @@ const slickPreset = {
     nextArrow: "<img src='../img/next.png' class='next' alt='right'>",
 }
 
+// слайдер для раздела всё включено
+const slickInclusive = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    prevArrow: "<img src='../img/arrows-inclusive-l.png' class='prev' alt='left'>",
+    nextArrow: "<img src='../img/arrows-inclusive-r.png' class='next' alt='right'>",
+}
+
+// слик слайдер
 $(document).ready(function(){
     $('.choice__slider1').slick(slickPreset);
     $('.choice__slider2').slick(slickPreset);
     $('.choice__slider3').slick(slickPreset);
+    $('.inclusive__slider').slick(slickInclusive);
 });
 
-
+// код для переключения табов
 const choiceBtn = document.querySelectorAll(".choice__tab-item");
 const choiceItem = document.querySelectorAll(".choice__tab-body-item");
 
@@ -41,3 +54,22 @@ function choiceOnTabClick(item) {
 }
 
 document.querySelector('.choice__tab-item').click();
+
+// код для меню
+const menuOpen = document.querySelector(".header__menu");
+const menuClose = document.querySelector(".header__menu-close");
+const menu = document.querySelector(".menu__popup");
+const header = document.querySelector(".header__wrp")
+
+menuOpen.addEventListener("click", function(){
+    menu.classList.add('menu__popup-active');
+    header.classList.add('header-active');
+    menu.classList.remove('menu__popup')
+});
+
+menuClose.addEventListener("click", function(){
+    menu.classList.add('menu__popup');
+    menu.classList.remove('menu__popup-active')
+    header.classList.remove('header-active');
+
+});
